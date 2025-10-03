c117cdde3f8ab14e1febe32a00bcbe6d22160ff3
joaopaulo10101010 <joao_paulo_oandrade@hotmail.com>
2025-10-02 21:53:36 -0300

    salvando

---
+++ PalestraPalestrante/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Generators;
+
+namespace PalestraPalestrante.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly Database db = new Database();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CriarUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CriarUsuario(Usuarios vm)
+        {
+            using var conn = db.GetConnection();
+            using var cmd = new MySqlCommand("sp_usuario_criar", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("p_nome", vm.nome);
+            cmd.Parameters.AddWithValue("p_email", vm.email);
+            cmd.Parameters.AddWithValue("p_senha_hash", BCrypt.Net.BCrypt.HashPassword(vm.senha_hash, workFactor: 12));
+            cmd.Parameters.AddWithValue("p_role", vm.role);
+            cmd.ExecuteNonQuery();
+            return RedirectToAction("CriarUsuario");
+        }
+    }
+}
---
+++ PalestraPalestrante/Database/Conexao.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace PalestraPalestrante.Database
+{
+    public class Conexao : IDisposable
+    {
+        
+            private MySqlConnection _connection;
+
+            // Abre a conexão
+
+            public Conexao(string connectionString)
+            {
+                _connection = new MySqlConnection(connectionString);
+                _connection.Open();
+            }
+
+            // Recebe os comandos do SQL
+
+            public MySqlCommand MySqlCommand()
+            {
+                return _connection.CreateCommand();
+            }
+
+            // Fecha a conexão
+
+            public void Dispose()
+            {
+                if (_connection != null && _connection.State == ConnectionState.Open)
+                {
+                    _connection.Close();
+                    _connection.Dispose();
+                }
+            }
+        
+    }
+}

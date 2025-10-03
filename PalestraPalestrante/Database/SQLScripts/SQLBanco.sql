ff58355379b107608b7d3f34e93892d0d951ec02
joaopaulo10101010 <joao_paulo_oandrade@hotmail.com>
2025-10-02 21:45:56 -0300

    subindo para repositorio

---
+++ PalestraPalestrante/Database/SQLScripts/SQLBanco.sql
@@ -0,0 +1,48 @@
+drop database db_palestra;
+create database db_palestra;
+use db_palestra;
+
+create table Eventos(
+	id_evento int auto_increment,
+    nome varchar(255) not null,
+    data date not null,
+    imagem_evento longblob,
+    primary key(id_evento)
+);
+
+create table AreaAtuacao(
+	id_area int auto_increment,
+    nome_area varchar(255),
+    primary key(id_area)
+);
+
+create table Palestrantes(
+	id_palestrante int auto_increment,
+    nome varchar(255) not null,
+    id_area int not null,
+    foto_perfil longblob,
+    data_cadastro datetime default current_timestamp,
+    primary key(id_palestrante),
+    foreign key(id_area) references AreaAtuacao(id_area)
+);
+
+create table Palestras(
+	id_palestra int auto_increment primary key,
+    id_evento int,
+    id_palestrante int,
+    descricao_palestra varchar(500),
+	id_area int,
+    foreign key(id_area) references AreaAtuacao(id_area),
+    foreign key(id_evento) references Eventos(id_evento),
+    foreign key(id_palestrante) references Palestrantes(id_palestrante)
+);
+
+create table Usuario(
+	cpf_usuario char(11),
+    nome_usuario varchar(255),
+    senha_usuario varchar(500),
+    cargo_usuario enum('Admin','Participante','Publico') default 'Publico',
+	data_cadastro_usuario datetime default current_timestamp,
+    primary key(cpf_usuario)
+);
+
